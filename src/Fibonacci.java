65fbb915827ad99bec117d628e3b657ecc4b400f
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-05-22 18:29:54 -0700

    Fibonacci, For-loopGauntlet

---
+++ src/Fibonacci.java
@@ -0,0 +1,14 @@
+
+public class Fibonacci {
+	public static void main(String[] args) {
+System.out.println("0,1,1,2,3,5,8,13,21,34,55,89,144");		
+int sum=0; 
+int add=1;
+int swap=1;		
+		for(int i=0; i<12; i++) {
+			
+		}
+		
+		
+	}
+}
--- src/ForLoopGauntlet.java
+++ src/ForLoopGauntlet.java
@@ -43,5 +43,35 @@ for(int i=7; i<778; i++) {
 for(int i=2005; i<2019; i++) {
 System.out.println(i-2005+","+ i);	
 }
+//#1.Nested for-loops
+for(int i=0; i<3; i++) {
+	for(int j=0; j<3; j++) {
+	System.out.println(i+"  "+j);
 	}
 }
+	//#2.Nested for-loops, 3x3 grid
+for(int i=1; i<9; i+=3) {
+	for(int j=i; j<i+3; j++) {
+		System.out.print(j+"  ");
+	
+}
+	System.out.println();
+}
+//#3.Nested for-loops, 100 in a 10x10 square grid
+for(int i=1; i<100; i+=10) {
+	for(int j=i; j<i+10; j++) {
+		System.out.print(j+" ");
+	}
+System.out.println();	
+}
+	
+//#4.Nested for-loops
+	for(int i=1; i<6 ; i++) {
+		for(int j=1; j<=i; j++) {
+			System.out.print(" *");
+		}
+	System.out.println();
+	}
+	
+	}
+	}
